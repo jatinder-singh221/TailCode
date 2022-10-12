7c21c55adaf267cf642789ab29838e2004213998
jatinder-singh221 <jatindersing19@gmail.com>
2022-10-13 00:45:55 +0530

    code review and bug fix

--- backend/notification/views.py
+++ backend/notification/views.py
@@ -4,7 +4,6 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.status import HTTP_400_BAD_REQUEST, HTTP_404_NOT_FOUND, HTTP_200_OK
 
-from .models import notifications
 from .serializers import notificationSerializer
 
 class getNotificationView(APIView):
